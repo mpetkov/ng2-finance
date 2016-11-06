871e7782769fe545d987a8bc1024edfe681e4c3b
Marin Petkov <marin@marinpetkov.com>
2016-11-06 10:43:34 -0600

    Changed the was component style sheets are loaded.

--- src/client/app/shared/sidebar/sidebar.component.ts
+++ src/client/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'mp-sidebar',
-  templateUrl: 'sidebar.component.html'
+  templateUrl: 'sidebar.component.html',
+  styleUrls: ['sidebar.component.css']
 })
 
 export class SidebarComponent { }
