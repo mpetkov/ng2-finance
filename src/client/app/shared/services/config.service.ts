eae7a9cc7172b6b85a6d3c8a87f4e98cd6256779
Marin Petkov <marin@marinpetkov.com>
2016-11-19 06:38:52 -0600

    Created a config service which will have all static values such as urls.

---
+++ src/client/app/shared/services/config.service.ts
@@ -0,0 +1,9 @@
+export class ConfigService {
+  static yqlUrl:string = 'https://query.yahooapis.com/v1/public/yql';
+
+  static queries():any {
+    return {
+      quotes: this.yqlUrl + '?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20($stocks)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    }
+  }
+}
