34284daea87c00c0d985ac0e9a39a62000bba2d7
Marin Petkov <marin@marinpetkov.com>
2016-11-20 12:22:20 -0600

    Added global styles which will be used for all components and Roboto font family.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import './operators';
 
 @Component({
   moduleId: module.id,
   selector: 'mp-app',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class AppComponent {
