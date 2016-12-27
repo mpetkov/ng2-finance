6160191a54bb988e5cfda1e9f0c1b01dbb87bd7d
Marin Petkov <marin@marinpetkov.com>
2016-12-26 20:43:46 -0600

    Made the toolbar change itself based on the selected action.

--- src/client/app/watchlist/sidebar/toolbar/toolbar.component.ts
+++ src/client/app/watchlist/sidebar/toolbar/toolbar.component.ts
@@ -12,16 +12,25 @@ import { SidebarActions, SidebarTypeEnum } from "../../../shared/index";
 
 export class ToolbarComponent {
   sidebarStore: Observable<any>;
+  title:string;
 
   constructor(private store: Store){
     this.sidebarStore = store.select(store => store.sidebar);
+    this.title = 'Stocks';
   }
 
   add(){
     this.store.dispatch(SidebarActions.type(SidebarTypeEnum.Add));
+    this.title = 'Add';
   }
 
   edit(){
     this.store.dispatch(SidebarActions.type(SidebarTypeEnum.Edit));
+    this.title = 'Edit';
+  }
+
+  close(){
+    this.store.dispatch(SidebarActions.type(SidebarTypeEnum.List));
+    this.title = 'Stocks';
   }
 }
