67847855164fb1a82627cb4bcf0f0c4a74c21dd0
Marin Petkov <marin@marinpetkov.com>
2016-12-26 20:47:28 -0600

    The list will only show up when the list type is selected.

--- src/client/app/watchlist/sidebar/sidebar.component.ts
+++ src/client/app/watchlist/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { SidebarStateInterface } from "../../shared/index";
 
 @Component({
   moduleId: module.id,
@@ -6,4 +9,10 @@ import { Component } from '@angular/core';
   templateUrl: 'sidebar.component.html'
 })
 
-export class SidebarComponent {}
+export class SidebarComponent {
+  sidebarStore: Observable<SidebarStateInterface>;
+
+  constructor(private store: Store){
+    this.sidebarStore = store.select(store => store.sidebar);
+  }
+}
