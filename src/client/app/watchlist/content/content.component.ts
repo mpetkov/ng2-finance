d97c864539f5a6c667ae5eabd78487dd492899a9
Marin Petkov <marin@marinpetkov.com>
2017-01-09 08:46:26 -0600

    Styled the cotent so that it takes up the entire space in the column.

--- src/client/app/watchlist/content/content.component.ts
+++ src/client/app/watchlist/content/content.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'mp-content',
-  templateUrl: 'content.component.html'
+  templateUrl: 'content.component.html',
+  styleUrls: ['content.component.css']
 })
 
 export class ContentComponent {
