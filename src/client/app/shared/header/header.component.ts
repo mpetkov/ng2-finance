bb9c999bca1ca8fe776a442e30eee11457aed7a8
Marin Petkov <marin@marinpetkov.com>
2017-01-10 23:38:27 -0600

    Changed styling of the main and toolbar headers.

--- src/client/app/shared/header/header.component.ts
+++ src/client/app/shared/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'mp-header',
-  templateUrl: 'header.component.html'
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class HeaderComponent { }
