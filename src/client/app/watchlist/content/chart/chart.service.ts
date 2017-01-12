fed46170174576b374a2c6f5b241f60066fd4b26
Marin Petkov <marin@marinpetkov.com>
2017-01-12 06:58:38 -0600

    Added parsing to the charts data to prepare it for d3.

--- src/client/app/watchlist/content/chart/chart.service.ts
+++ src/client/app/watchlist/content/chart/chart.service.ts
@@ -20,16 +20,35 @@ export class ChartsService extends LoaderService {
       );
   }
 
-  private transform(data:any):any {
-    let chartData:any = _.get(data, 'chart.result[0]', {});
-    return {
-      close: _.get(chartData, 'indicators.quote[0].close', []),
-      high: _.get(chartData, 'indicators.quote[0].high', []),
-      low: _.get(chartData, 'indicators.quote[0].low', []),
-      open: _.get(chartData, 'indicators.quote[0].open', []),
-      volume: _.get(chartData, 'indicators.quote[0].volume', []),
-      info: chartData.meta || {},
-      dates: chartData.timestamp || []
+  private transform(rawData:any):any {
+    let data:any = {
+      info: {},
+      rows: []
     };
+    let chartData:any = _.get(rawData, 'chart.result[0]', {});
+    if(chartData) {
+      data.info = chartData.meta || {};
+      let items:any = {
+        close: _.get(chartData, 'indicators.quote[0].close', []),
+        high: _.get(chartData, 'indicators.quote[0].high', []),
+        low: _.get(chartData, 'indicators.quote[0].low', []),
+        open: _.get(chartData, 'indicators.quote[0].open', []),
+        volume: _.get(chartData, 'indicators.quote[0].volume', []),
+        dates: chartData.timestamp || []
+      };
+
+      items.dates.forEach((value:number, index:number) => {
+        data.rows.push({
+          timestamp: value,
+          date: new Date(value * 1000),
+          close: _.get(items, 'close[' + index + ']', null),
+          high: _.get(items, 'high[' + index + ']', null),
+          low: _.get(items, 'low[' + index + ']', null),
+          open: _.get(items, 'open[' + index + ']', null),
+          volume: _.get(items, 'volume[' + index + ']', null),
+        });
+      });
+    }
+    return data;
   }
 }
