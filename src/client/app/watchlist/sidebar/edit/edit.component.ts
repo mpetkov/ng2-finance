0900b08ce5275c8e8fb1551542cffca5bd9793cb
Marin Petkov <marin@marinpetkov.com>
2017-01-18 22:41:07 -0600

    Added delete buttons and actions to the edit list.

--- src/client/app/watchlist/sidebar/edit/edit.component.ts
+++ src/client/app/watchlist/sidebar/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer, OnDestroy } from '@angular/core';
 import { StocksStateService } from '../stocks/state/stocks-state.service';
 
 @Component({
@@ -8,7 +8,33 @@ import { StocksStateService } from '../stocks/state/stocks-state.service';
   styleUrls: ['edit.component.css']
 })
 
-export class EditComponent {
-  constructor(public stocksState:StocksStateService) {
+export class EditComponent implements OnDestroy {
+  deleteSymbol:string;
+  private windowClickListener: Function;
+
+  constructor(public stocksState:StocksStateService,
+              private renderer:Renderer) {
+  }
+
+  showDelete(symbol:string, event:any) {
+    this.deleteSymbol = symbol;
+    event.stopPropagation();
+    this.windowClickListener = this.renderer.listenGlobal('window', 'click', (event:any) => {this.hideDelete();});
+  }
+
+  delete(symbol:string) {
+    //Delete logic will go here
+  }
+
+  ngOnDestroy() {
+    this.hideDelete();
+  }
+
+  private hideDelete(){
+    this.deleteSymbol = null;
+    if(this.windowClickListener) {
+      this.windowClickListener();
+      this.windowClickListener = null;
+    }
   }
 }
