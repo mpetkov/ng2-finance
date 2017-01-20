cd711947b3396bd24897bbbc3baae2f0ad152b3a
Marin Petkov <marin@marinpetkov.com>
2017-01-20 07:49:36 -0600

    Loaded and displayed the news list.

--- src/client/app/watchlist/content/news/news.component.ts
+++ src/client/app/watchlist/content/news/news.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
+import { NewsStateService } from './state/index';
+import { SidebarStateService } from '../../sidebar/state/index';
 
 @Component({
   moduleId: module.id,
   selector: 'mp-news',
-  templateUrl: 'news.component.html'
+  templateUrl: 'news.component.html',
+  styleUrls: ['news.component.css'],
+  providers: [NewsApiService, NewsStateService]
 })
 
 export class NewsComponent {
+  constructor(public newsState:NewsStateService,
+              private sidebarState:SidebarStateService,
+              private newsApiService:NewsApiService) {
+    sidebarState.stock$.subscribe(
+      stock => newsApiService.load(stock)
+    );
+  }
 }
