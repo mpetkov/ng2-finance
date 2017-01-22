5e9ddd28e1ead9f443ae146f0406ee89b537f772
Marin Petkov <marin@marinpetkov.com>
2017-01-22 06:05:44 -0600

    Added autocomplete results and an action to add a stock when the user picks a result.

--- src/client/app/watchlist/sidebar/add/add.component.ts
+++ src/client/app/watchlist/sidebar/add/add.component.ts
@@ -1,19 +1,28 @@
-import { Component, Renderer, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { SidebarStateService, SidebarTypeEnum } from '../state/index';
+import { SearchApiService } from './search-api.service';
 
 @Component({
   moduleId: module.id,
   selector: 'mp-add',
   templateUrl: 'add.component.html',
-  styleUrls: ['add.component.css']
+  styleUrls: ['add.component.css'],
+  providers: [SearchApiService]
 })
 
 export class AddComponent {
-  constructor(public sidebarState:SidebarStateService) {
+  constructor(public sidebarState:SidebarStateService,
+              public searchApiService:SearchApiService) {
   }
 
   updateSearch(value:string) {
-    
+    this.searchApiService.load(value);
+  }
+
+  add(stock:any) {
+    this.sidebarState.addStock(stock.symbol);
+    this.sidebarState.changeType(SidebarTypeEnum.List);
+    this.sidebarState.changeStock(stock);
   }
 
   close() {
