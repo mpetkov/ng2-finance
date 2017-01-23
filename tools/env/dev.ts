412705256214a12d9b6d0ec65ce450e69517b24c
Marin Petkov <marin@marinpetkov.com>
2017-01-23 07:47:06 -0600

    Changed the dev settings to use the local stocks json file.

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -9,9 +9,7 @@ const DevConfig: ConfigInterface = {
     news: './assets/json/news.json',
     proxy: 'http://www.marinpetkov.com/proxy.php',
     search: './assets/json/search.json',
-    stocks: yqlUrl +
-    '?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20($stocks)' +
-    '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    stocks: './assets/json/stocks.json',
   }
 };
 
