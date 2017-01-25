32addc1d1d4e03ddb035474ab996fa2dccac2b70
Marin Petkov <marin@marinpetkov.com>
2017-01-25 00:23:36 -0600

    Changed loader error message.

--- src/client/app/shared/services/loader.service.ts
+++ src/client/app/shared/services/loader.service.ts
@@ -27,8 +27,6 @@ export class LoaderService {
   }
 
   private handleError(error:any) {
-    let errMsg:string = (error.message) ? error.message :
-      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    return Observable.throw(errMsg);
+    return Observable.throw('Unable to load API.  Please try reloading the page.');
   }
 }
