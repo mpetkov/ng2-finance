9513ccd4067a112fe17ef780686005dfd072c1be
Marin Petkov <marin@marinpetkov.com>
2017-01-26 05:22:29 -0600

    Made the search api dynamic.

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -16,7 +16,7 @@ const ProdConfig: ConfigInterface = {
     'symbol%22%2C%22suggestionMeta%22%3A%5B%22symbol%22%5D%2C%22url%22%3A%7B%22protocol%22%3A%22https%22%2C%22host%22%3A%22' +
     's.yimg.com%22%2C%22path%22%3A%22%2Fxb%2Fv6%2Ffinance%2Fautocomplete%22%2C%22query%22%3A%7B%22appid%22%3A%22yahoo.com' +
     '%22%2C%22nresults%22%3A10%2C%22output%22%3A%22yjsonp%22%2C%22region%22%3A%22US%22%2C%22lang%22%3A%22en-US' +
-    '%22%7D%7D%2C%22region%22%3A%22US%22%2C%22lang%22%3A%22en-US%22%7D;searchTerm=ya?bkt=%5B%22findd-test03' +
+    '%22%7D%7D%2C%22region%22%3A%22US%22%2C%22lang%22%3A%22en-US%22%7D;searchTerm=$stock?bkt=%5B%22findd-test03' +
     '%22%2C%22findmexp002%22%5D&device=desktop&feature=canvass%2CnewContentAttribution%2CrelatedVideoFeature' +
     '&intl=us&lang=en-US&partner=none&region=US&site=finance&tz=America%2FChicago&ver=0.101.749&returnMeta=true&callback=quote',
     stocks: yqlUrl +
