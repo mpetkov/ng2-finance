c4b0f77404dd34e40af9458ce118fd00e7f2bfa6
Marin Petkov <marin@marinpetkov.com>
2017-02-06 06:05:28 -0600

    Removed the 1 month range selector.

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -10,7 +10,6 @@ const BaseConfig: ConfigInterface = {
   chartRanges: [
     {id: '1d', text: '1d', interval: '1m'},
     {id: '5d', text: '1w', interval: '5m'},
-    {id: '1mo', text: '1m', interval: '1d'},
     {id: '3mo', text: '3m', interval: '1d'},
     {id: '6mo', text: '6m', interval: '1d'},
     {id: '1y', text: '1y', interval: '1d'},
