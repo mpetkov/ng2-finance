b709618db3e8e14d581da595b5a01d161bf9db06
Marin Petkov <marin@marinpetkov.com>
2017-02-08 17:35:26 -0600

    Changed chart api to use the csv historic data file.

--- src/client/app/watchlist/content/chart/chart-api.service.ts
+++ src/client/app/watchlist/content/chart/chart-api.service.ts
@@ -7,6 +7,7 @@ import {
 } from '../../../core/index';
 import { ChartStateService } from './state/index';
 declare let _:any;
+declare let moment:any;
 
 @Injectable()
 export class ChartApiService extends CoreApiResponseService {
@@ -19,52 +20,35 @@ export class ChartApiService extends CoreApiResponseService {
   load(stock:string, range:string, interval:string) {
     this.params = {stock:stock, range:range, interval:interval};
     this.chartState.fetchLoader(true);
-    if(Config.env === 'PROD') {
-      let url:string = Config.paths.charts.replace('$stock', stock);
-      url = url.replace('$range', range);
-      url = url.replace('$interval', interval);
-      this.post(Config.paths.proxy, 'url=' + encodeURIComponent(url))
-        .subscribe(
-          data => this.complete(data),
-          error => this.chartState.fetchError(error)
-        );
-    } else {
-      this.get(Config.paths.charts)
-        .subscribe(
-          data => this.complete(data),
-          error => this.chartState.fetchError(error)
-        );
-    }
+
+    let url:string = Config.paths.charts.replace('$stock', stock);
+    url = url.replace('$range', range);
+    url = url.replace('$interval', interval);
+    this.get(Config.paths.charts, LoaderDataTypeEnum.CSV)
+      .subscribe(
+        data => this.complete(this.transform(data)),
+        () => this.failed()
+      );
   }
 
   reload() {
     this.load(this.params.stock, this.params.range, this.params.interval);
   }
 
-
-  private transform(rawData:any):any {
-    let data:any = [];
-
-    let chartData:any = _.get(rawData, 'chart.result[0]', {});
-    if (chartData) {
-      let items:any = {
-        close: _.get(chartData, 'indicators.quote[0].close', []),
-        high: _.get(chartData, 'indicators.quote[0].high', []),
-        low: _.get(chartData, 'indicators.quote[0].low', []),
-        open: _.get(chartData, 'indicators.quote[0].open', []),
-        volume: _.get(chartData, 'indicators.quote[0].volume', []),
-        dates: chartData.timestamp || []
-      };
-
-      items.dates.forEach((value:number, index:number) => {
+  private transform(rawData:any):any[] {
+    let data:any[] = [];
+    if (rawData) {
+      let timestamp:number;
+      rawData.forEach((row:string[]) => {
+        timestamp = moment(row[0], 'YYYY-MM-DD').unix();
         data.push({
-          timestamp: value,
-          date: new Date(value * 1000),
-          close: _.get(items, 'close[' + index + ']', null),
-          high: _.get(items, 'high[' + index + ']', null),
-          low: _.get(items, 'low[' + index + ']', null),
-          open: _.get(items, 'open[' + index + ']', null),
-          volume: _.get(items, 'volume[' + index + ']', null),
+          timestamp: timestamp,
+          date: new Date(timestamp * 1000),
+          close: Number(row[4]),
+          high: Number(row[2]),
+          low: Number(row[3]),
+          open: Number(row[1]),
+          volume: Number(row[5])
         });
       });
     }
