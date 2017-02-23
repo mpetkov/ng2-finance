069b935650251568ba86d30152fcf5b0cf249993
Marin Petkov <marin@marinpetkov.com>
2017-02-23 08:15:06 -0600

    Added 100% test coverage to the news component.

--- src/client/app/watchlist/content/news/news.component.spec.ts
+++ src/client/app/watchlist/content/news/news.component.spec.ts
@@ -7,8 +7,8 @@ import {
 } from '@angular/core/testing';
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { StoreModule } from '@ngrx/store';
-import { Subject } from 'rxjs/Subject';
 import { MdlModule } from 'angular2-mdl';
 import {
   NewsComponent,
@@ -33,15 +33,28 @@ export function main() {
     let fixture:ComponentFixture<NewsComponent>;
     let component:NewsComponent;
     let api:any;
-    let getSubject:Subject<any>;
-    let postSubject:Subject<any>;
+    let newsState:any;
+    let watchlistState:any;
 
     beforeEach(async(() => {
-      getSubject = new Subject<any>();
-      postSubject = new Subject<any>();
-      api = jasmine.createSpyObj('api', ['get', 'post']);
-      api.get.and.callFake(() => getSubject);
-      api.post.and.callFake(() => postSubject);
+      api = jasmine.createSpyObj('api', [
+        'load',
+        'reload'
+      ]);
+
+      newsState = jasmine.createSpyObj('newsStateService', [
+        'fetchFulfilled'
+      ]);
+
+      newsState.data$ = new BehaviorSubject<any>([]);
+      newsState.loader$ = new BehaviorSubject<any>(false);
+      newsState.error$ = new BehaviorSubject<any>(null);
+
+      watchlistState = jasmine.createSpyObj('watchlistStateService', [
+        'changeStock'
+      ]);
+
+      watchlistState.stock$ = new BehaviorSubject<any>(null);
 
       TestBed.configureTestingModule({
         imports: [
@@ -58,8 +71,8 @@ export function main() {
         ],
         providers: [
           {provide: NewsApiService, useValue: api},
-          NewsStateService,
-          WatchlistStateService
+          {provide: NewsStateService, useValue: newsState},
+          {provide: WatchlistStateService, useValue: watchlistState}
         ]
       }).compileComponents();
     }));
@@ -72,6 +85,54 @@ export function main() {
 
     it('should create', () => {
       expect(component).toBeTruthy();
+    });
+
+    it('should have a NotificationComponent', () => {
+      expect(fixture.nativeElement.querySelector('mp-notification')).not.toBeNull();
+    });
+
+    it('should have a title', () => {
+      expect(fixture.nativeElement.querySelector('h4').textContent).toBe('News');
+    });
+
+    it('should show the more settings icon when the notification type is set to 0', () => {
+      expect(fixture.nativeElement.querySelector('.mp-settings')).toBeNull();
+
+      component.notificationType = 0;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('.mp-settings')).not.toBeNull();
+    });
+
+    it('should show the content list when the notification type is set to 0', () => {
+      expect(fixture.nativeElement.querySelector('.mdl-list')).toBeNull();
+
+      component.notificationType = 0;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('.mdl-list')).not.toBeNull();
+    });
+
+    it('should call NewsApiService#reload() when reload menu item is clicked', () => {
+      watchlistState.stock$.next('test');
+      component.notificationType = 0;
+      fixture.detectChanges();
+      fixture.nativeElement.querySelector('.mdl-menu__item').click();
+      expect(api.reload).toHaveBeenCalledTimes(1);
+    });
+
+    it('should change `notification` to have the correct message when there is no data', () => {
+      component.notification = null;
+      newsState.data$.next([{}]);
+      fixture.detectChanges();
+      expect(component.notification).toBeNull();
+
+      newsState.data$.next([]);
+      fixture.detectChanges();
+      expect(component.notification).toBe('Please select a stock symbol');
+
+      watchlistState.stock$.next('test');
+      newsState.data$.next([]);
+      fixture.detectChanges();
+      expect(component.notification).toBe('No results found');
     });
   });
 }
