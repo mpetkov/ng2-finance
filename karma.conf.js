8c31fc6891c18b54c90ef3a236ade3ad5ae4a447
Marin Petkov <marin@marinpetkov.com>
2017-03-08 06:49:30 -0600

    Added moment library to the karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -43,6 +43,7 @@ module.exports = function (config) {
       'node_modules/d3/d3.min.js',
       'node_modules/d3fc/dist/d3fc.min.js',
       'node_modules/lodash/lodash.min.js',
+      'node_modules/moment/min/moment.min.js',
 
       // RxJs.
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
