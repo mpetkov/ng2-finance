9eb5f82c366838d30340a8b9be8855a0da5718c7
Marin Petkov <marin@marinpetkov.com>
2017-06-01 08:03:37 -0500

    Updated travis file to work with new system.

--- karma.conf.js
+++ karma.conf.js
@@ -39,6 +39,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
