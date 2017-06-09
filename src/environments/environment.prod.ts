f011a472160a59b8df810bae8a60e51ec5dba6d0
Marin Petkov <marin@marinpetkov.com>
2017-06-09 17:15:06 -0500

    Added a base environment file where shared data will be stored.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,7 @@
-export const environment = {
+import {environment as base} from './environment.base';
+import {extend} from 'lodash';
+export const environment = extend(base, {
   production: true,
-  notifications: {
-    noData: 'No results found',
-    noStock: 'Please select a stock symbol',
-    search: 'Type a company name or stock symbol',
-    noFavorites: 'Your favorites is empty'
-  },
-  chartRanges: [
-    {id: '1d', text: '1d', interval: '1m'},
-    {id: '5d', text: '1w', interval: '5m'},
-    {id: '3mo', text: '3m', interval: '1d'},
-    {id: '6mo', text: '6m', interval: '1d'},
-    {id: '1y', text: '1y', interval: '1d'},
-    {id: '5y', text: '5y', interval: '1wk'},
-    {id: 'max', text: 'max', interval: '1mo'}
-  ],
   paths: {
     charts: './assets/json/chart/$stock/$range.json',
     news: './assets/json/news/$stock.json',
@@ -23,4 +10,4 @@ export const environment = {
     stocks: './assets/json/stocks.json',
     info: './assets/json/info/$stock.json'
   }
-};
+});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,8 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+import {environment as base} from './environment.base';
+import {extend} from 'lodash';
 
-export const environment = {
+export const environment = extend(base, {
   production: false,
-  notifications: {
-    noData: 'No results found',
-    noStock: 'Please select a stock symbol',
-    search: 'Type a company name or stock symbol',
-    noFavorites: 'Your favorites is empty'
-  },
-  chartRanges: [
-    {id: '1d', text: '1d', interval: '1m'},
-    {id: '5d', text: '1w', interval: '5m'},
-    {id: '3mo', text: '3m', interval: '1d'},
-    {id: '6mo', text: '6m', interval: '1d'},
-    {id: '1y', text: '1y', interval: '1d'},
-    {id: '5y', text: '5y', interval: '1wk'},
-    {id: 'max', text: 'max', interval: '1mo'}
-  ],
   paths: {
     charts: './assets/json/chart/$stock/$range.json',
     news: './assets/json/news/$stock.json',
@@ -28,4 +11,4 @@ export const environment = {
     stocks: './assets/json/stocks.json',
     info: './assets/json/info/$stock.json'
   }
-};
+});
